c775575c9b400c90628439fae82a247854a4ddcd
amineen <amineen64@hotmail.com>
2022-07-20 18:01:08 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log(`http://localhost.com`));
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    msg: 'Testing Stackblits.com',
+    version: '1.0.0',
+    developer: 'Aaron Mineen',
+  });
+});
+
+app.listen(3000, () => console.log(`http://localhost:3000`));
